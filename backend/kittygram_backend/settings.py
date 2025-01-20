73b5dd896a111f25b5c6100fc11b84c29fa8148e
kykit <kykitos@yandex.ru>
2025-01-21 01:04:03 +0300

    update urls.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('TOKEN', 'django-insecure-123')
+SECRET_KEY = os.getenv('TOKEN', 'django-insecure')
 
 DEBUG = os.getenv('DEBUG', '').lower() == 'true'
 
